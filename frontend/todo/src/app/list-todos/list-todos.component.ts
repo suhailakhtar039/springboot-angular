8c2060f1e9ea6eb9ca0e5f458e541520ec616078
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-25 12:51:24 +0530

    structure for list todos created

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,12 +1,16 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list-todos',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './list-todos.component.html',
-  styleUrl: './list-todos.component.css'
+  styleUrl: './list-todos.component.css',
 })
 export class ListTodosComponent {
-
+  todo = {
+    id: 1,
+    description: 'Learn to Dance',
+  };
 }
