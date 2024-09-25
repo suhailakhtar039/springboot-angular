3d80e742285b997203c79de90524f7387b794089
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-25 13:47:09 +0530

    menu and footer component added

--- frontend/todo/src/app/app.component.ts
+++ frontend/todo/src/app/app.component.ts
@@ -3,11 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomeComponent, LoginComponent, ErrorComponent],
+  imports: [
+    RouterOutlet,
+    WelcomeComponent,
+    LoginComponent,
+    ErrorComponent,
+    MenuComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
