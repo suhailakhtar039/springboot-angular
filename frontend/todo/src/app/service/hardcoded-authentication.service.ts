ec7926d40c3ea4d7b3dc55c0282d545ab4ad1024
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-25 23:31:26 +0530

    using session storage

--- frontend/todo/src/app/service/hardcoded-authentication.service.ts
+++ frontend/todo/src/app/service/hardcoded-authentication.service.ts
@@ -8,8 +8,13 @@ export class HardcodedAuthenticationService {
 
   authenticate(username: string, password: string) {
     if (username === 'suhail' && password === 'abc') {
+      sessionStorage.setItem('authenticaterUser', username);
       return true;
     }
     return false;
+  }
+
+  isUserLoggedIn() {
+    return !sessionStorage.getItem('authenticaterUser');
   }
 }
