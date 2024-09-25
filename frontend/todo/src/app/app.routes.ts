ba4b985b0d752162a18c8d80187475add0af8844
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-25 10:24:38 +0530

    path created for login and welcome component

--- frontend/todo/src/app/app.routes.ts
+++ frontend/todo/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent },
+];
