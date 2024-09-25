a71a1feb68029c5f5859bf56389711a1848076aa
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-25 23:05:13 +0530

    replaced href with routerLink

--- frontend/todo/src/app/menu/menu.component.ts
+++ frontend/todo/src/app/menu/menu.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
 })
-export class MenuComponent {
-
-}
+export class MenuComponent {}
