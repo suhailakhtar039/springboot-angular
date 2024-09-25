b18e5ad09651959b17aace9cc2446e3be863a487
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-25 22:55:33 +0530

    login created

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -12,7 +12,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
   username = 'suhail';
-  password = 'abc';
+  password = '';
   errorMessage = 'invalid credentials';
   invalidLogin = false;
 
