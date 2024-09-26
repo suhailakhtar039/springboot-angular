551a5c23c55b2dc22788ceb46c8654f7d1e0d4d2
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-26 12:24:03 +0530

    login is working using session storage

--- frontend/todo/src/app/menu/menu.component.ts
+++ frontend/todo/src/app/menu/menu.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
-export class MenuComponent {}
+export class MenuComponent implements OnInit {
+  // isUserLoggedIn: boolean = false;
+
+  constructor(
+    public hardcodedAuthenticationService: HardcodedAuthenticationService
+  ) {}
+
+  ngOnInit(): void {
+    // this.isUserLoggedIn = this.hardcodedAuthenticationService.isUserLoggedIn();
+  }
+}
--- frontend/todo/src/app/service/hardcoded-authentication.service.ts
+++ frontend/todo/src/app/service/hardcoded-authentication.service.ts
@@ -1,20 +1,31 @@
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HardcodedAuthenticationService {
-  constructor() {}
+  constructor(@Inject(PLATFORM_ID) private _platformId: Object) {}
 
   authenticate(username: string, password: string) {
     if (username === 'suhail' && password === 'abc') {
-      sessionStorage.setItem('authenticaterUser', username);
+      sessionStorage.setItem('authenticatedUser', username);
       return true;
     }
     return false;
   }
 
   isUserLoggedIn() {
-    return !sessionStorage.getItem('authenticaterUser');
+    if (isPlatformBrowser(this._platformId)) {
+      console.log('Before session');
+      const user = sessionStorage.getItem('authenticatedUser');
+
+      return !(user == null);
+    }
+    return null;
+  }
+
+  logout() {
+    sessionStorage.removeItem('authenticaterUser');
   }
 }
