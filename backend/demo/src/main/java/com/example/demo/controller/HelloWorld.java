243eb438da30cc55b27682a50eaa80757612fb02
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-27 18:08:06 +0530

    added a bean

--- backend/demo/src/main/java/com/example/demo/controller/HelloWorld.java
+++ backend/demo/src/main/java/com/example/demo/controller/HelloWorld.java
@@ -1,18 +1,24 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
     @GetMapping("/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "<h1> Hello World </h1>";
     }
 
     @GetMapping("/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
-        return  new HelloWorldBean("Hello from bean");
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello from bean");
+    }
+
+    @GetMapping("/hello-world-bean/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean("Hello from bean " + name);
     }
 }
