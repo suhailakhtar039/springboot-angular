af3d3e3665612329d5a15618bf5405d68720d3ef
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-29 13:26:33 +0530

    todo hardcoded services created

---
+++ backend/demo/src/main/java/com/example/demo/todo/TodoHardcodedService.java
@@ -0,0 +1,20 @@
+package com.example.demo.todo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class TodoHardcodedService {
+
+    private static List<Todo> todos = new ArrayList<>();
+    private static int idCounter = 0;
+    static {
+        todos.add(new Todo(++idCounter, "suhail akhtar", "Learn to code good", new Date(), false));
+        todos.add(new Todo(++idCounter, "Hello there", "Revise more about microservices", new Date(), false));
+        todos.add(new Todo(++idCounter, "Oi Let us come", "Visit India", new Date(), false));
+    }
+    public List<Todo> findAll(){
+        return todos;
+    }
+
+}
