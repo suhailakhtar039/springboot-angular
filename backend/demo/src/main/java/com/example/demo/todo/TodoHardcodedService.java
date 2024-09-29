fbe1a2a4348b6de09ae5c6cfe186a49551058c3c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-29 15:45:26 +0530

    hardcoded service implemented

--- backend/demo/src/main/java/com/example/demo/todo/TodoHardcodedService.java
+++ backend/demo/src/main/java/com/example/demo/todo/TodoHardcodedService.java
@@ -1,9 +1,12 @@
 package com.example.demo.todo;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Service
 public class TodoHardcodedService {
 
     private static List<Todo> todos = new ArrayList<>();
