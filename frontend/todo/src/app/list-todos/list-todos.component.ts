46d70f02d28c874e9924bd32f447ae7a864ff1f0
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-29 17:16:20 +0530

    used subscribe for list todos

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,6 @@
 import { DatePipe, NgFor, NgIf, UpperCasePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 export class Todo {
   constructor(
@@ -17,13 +18,27 @@ export class Todo {
   templateUrl: './list-todos.component.html',
   styleUrl: './list-todos.component.css',
 })
-export class ListTodosComponent {
-  todos = [
-    new Todo(1, 'Learn to dance', false, new Date()),
-    new Todo(2, 'Become expert in angular', false, new Date()),
-    new Todo(3, 'Visit India', false, new Date()),
-    // { id: 1, description: 'Learn to Dance' },
-    // { id: 2, description: 'Become expert in angular' },
-    // { id: 3, description: 'Visit India' },
-  ];
+export class ListTodosComponent implements OnInit {
+  todos: Todo[] = [];
+  // [
+  //   new Todo(1, 'Learn to dance', false, new Date()),
+  //   new Todo(2, 'Become expert in angular', false, new Date()),
+  //   new Todo(3, 'Visit India', false, new Date()),
+  //   // { id: 1, description: 'Learn to Dance' },
+  //   // { id: 2, description: 'Become expert in angular' },
+  //   // { id: 3, description: 'Visit India' },
+  // ];
+
+  constructor(private todoService: TodoDataService) {}
+  ngOnInit(): void {
+    this.todoService.retrieveAllTodos('suhail-from-list-todos').subscribe(
+      (response) => {
+        // console.log(response);
+        this.todos = response;
+      },
+      (err) => {
+        console.log('in error in list todos');
+      }
+    );
+  }
 }
--- frontend/todo/src/app/service/data/todo-data.service.ts
+++ frontend/todo/src/app/service/data/todo-data.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Todo } from '../../list-todos/list-todos.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoDataService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  retrieveAllTodos(username: string) {
+    return this.http.get<Todo[]>(
+      `http://localhost:8080/users/${username}/todos`
+    );
+  }
 }
