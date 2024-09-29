2e74b317888ce279dac5205a3c7fed15d82bb2de
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-29 13:21:09 +0530

    todo resource created

---
+++ backend/demo/src/main/java/com/example/demo/todo/TodoResource.java
@@ -0,0 +1,8 @@
+package com.example.demo.todo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoResource {
+    private TodoHardcodedService todoService;
+}
