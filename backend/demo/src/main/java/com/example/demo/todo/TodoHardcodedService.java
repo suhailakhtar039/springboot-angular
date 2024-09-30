7c405c54071ce085333a544d6ec55708e39ef92b
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-09-30 23:31:02 +0530

    todo hardcoded service created

--- backend/demo/src/main/java/com/example/demo/todo/TodoHardcodedService.java
+++ backend/demo/src/main/java/com/example/demo/todo/TodoHardcodedService.java
@@ -41,5 +41,15 @@ public class TodoHardcodedService {
         return null;
     }
 
+    public Todo save(Todo todo){
+        if(todo.getId() == -1){
+            todo.setId(++idCounter);
+            todos.add(todo);
+        } else{
+            deleteById(todo.getId());
+            todos.add(todo);
+        }
+        return todo;
+    }
 
 }
