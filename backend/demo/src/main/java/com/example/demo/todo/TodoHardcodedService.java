14a1e8d1c505e6071a95f2d29e2f6ea522c8cb01
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-01 12:29:13 +0530

    zero added for check

--- backend/demo/src/main/java/com/example/demo/todo/TodoHardcodedService.java
+++ backend/demo/src/main/java/com/example/demo/todo/TodoHardcodedService.java
@@ -42,7 +42,7 @@ public class TodoHardcodedService {
     }
 
     public Todo save(Todo todo){
-        if(todo.getId() == -1){
+        if(todo.getId() == -1 || todo.getId() == 0){
             todo.setId(++idCounter);
             todos.add(todo);
         } else{
