d420330c78ff78ad01bde17bc8d744920e8069ed
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-02 22:58:22 +0530

    login completed

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
+import { BasicAuthenticationService } from '../service/basic-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -19,7 +20,8 @@ export class LoginComponent {
 
   constructor(
     private router: Router,
-    private hardcodedAuthenticationService: HardcodedAuthenticationService
+    private hardcodedAuthenticationService: HardcodedAuthenticationService,
+    private basicAuthenticationService: BasicAuthenticationService
   ) {}
 
   handleLogin() {
@@ -34,5 +36,21 @@ export class LoginComponent {
     } else {
       this.invalidLogin = true;
     }
+  }
+
+  handleBasicAuthLogin() {
+    this.basicAuthenticationService
+      .executeHelloWorldServiceWithPathVariable(this.username, this.password)
+      .subscribe(
+        (data) => {
+          console.log(data);
+          this.router.navigate(['welcome', this.username]);
+          this.invalidLogin = false;
+        },
+        (error) => {
+          console.log(error);
+          this.invalidLogin = true;
+        }
+      );
   }
 }
