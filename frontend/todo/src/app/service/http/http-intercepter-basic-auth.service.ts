f0194e690e1d1bb8ac114791572a4914084c07e5
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-02 12:32:55 +0530

    interceptor completed

--- frontend/todo/src/app/service/http/http-intercepter-basic-auth.service.ts
+++ frontend/todo/src/app/service/http/http-intercepter-basic-auth.service.ts
@@ -1,9 +1,34 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class HttpIntercepterBasicAuthService {
+export class HttpIntercepterBasicAuthService implements HttpInterceptor {
+  constructor() {}
 
-  constructor() { }
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    let username = 'user';
+    let password = 'password';
+
+    let basicAuthHeaderString =
+      'Basic ' + window.btoa(username + ':' + password);
+
+    request = request.clone({
+      setHeaders: {
+        Authorization: basicAuthHeaderString,
+      },
+    });
+
+    return next.handle(request);
+  }
 }
