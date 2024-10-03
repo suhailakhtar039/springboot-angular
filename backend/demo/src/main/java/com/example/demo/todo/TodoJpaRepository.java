6080d59cd5aaf0af7c2f224aa1c7673e46adac7f
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-03 23:35:18 +0530

    Todo JPA repository created

--- backend/demo/src/main/java/com/example/demo/todo/TodoJpaRepository.java
+++ backend/demo/src/main/java/com/example/demo/todo/TodoJpaRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoJpaRepository extends JpaRepository<Todo, Long> {
 }
