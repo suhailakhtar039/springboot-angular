2ab8b88cff249b850c7f0233138f272b40484145
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-03 23:08:39 +0530

    sql file created

---
+++ backend/demo/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into todo(id, username, description, target_date, is_done)
+values
+(1001, 'suhail', 'learn to code', current_date(), false),
+(1002, 'akhtar', 'learn to be happy', current_date(), false),
+(1003, 'suhail akhtar', 'gotta run', current_date(), false);
