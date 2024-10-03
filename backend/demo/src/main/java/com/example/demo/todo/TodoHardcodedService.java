61fa7beaca0350902db9ff279846864c93f7dbc6
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-03 22:47:42 +0530

    changes made according to entity

--- backend/demo/src/main/java/com/example/demo/todo/TodoHardcodedService.java
+++ backend/demo/src/main/java/com/example/demo/todo/TodoHardcodedService.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class TodoHardcodedService {
 
     private static List<Todo> todos = new ArrayList<>();
-    private static int idCounter = 0;
+    private static long idCounter = 0;
     static {
         todos.add(new Todo(++idCounter, "suhail akhtar", "Learn to code good", new Date(), false));
         todos.add(new Todo(++idCounter, "Hello there", "Revise more about microservices", new Date(), false));
