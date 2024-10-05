73736389d9bc4b7dd5a51d765af8b8e2a6e243d0
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-05 22:47:27 +0530

    csrf token added

--- SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/resource/TodoResource.java
+++ SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/resource/TodoResource.java
@@ -1,4 +1,34 @@
 package com.springsecurity.SpringSecurity.resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class TodoResource {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private static final List<Todo> TODOS_LIST = List.of(
+            new Todo("suhail","main user who will handle everything"),
+                new Todo("akhtar", "let bygones be bygones"));
+    @GetMapping("/todos")
+    public List<Todo> retrieveAllTodos(){
+        return TODOS_LIST;
+    }
+
+    @GetMapping("/users/{username}/todos")
+    public Todo retrieveTodoForUser(@PathVariable String username){
+
+        return TODOS_LIST.get(0);
+    }
+    @PostMapping("/users/{username}/todos")
+    public void createTodoForUser(@PathVariable String username, @RequestBody Todo todo){
+        logger.info("Create {} for {}", todo, username);
+    }
+
 }
+record Todo(String username, String description){}
