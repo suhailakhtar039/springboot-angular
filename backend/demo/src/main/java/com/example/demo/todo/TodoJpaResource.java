e7091fd66a7277bafe018e6a288d103ffa9bab12
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-05 13:07:38 +0530

    made changes in username

--- backend/demo/src/main/java/com/example/demo/todo/TodoJpaResource.java
+++ backend/demo/src/main/java/com/example/demo/todo/TodoJpaResource.java
@@ -11,52 +11,59 @@ import java.util.List;
 
 @CrossOrigin("http://localhost:4200/")
 @RestController
-public class TodoResource {
+public class TodoJpaResource {
 
     @Autowired
     private TodoHardcodedService todoService;
 
-    @GetMapping("/users/{username}/todos")
+    @Autowired
+    private TodoJpaRepository todoJpaRepository;
+
+    @GetMapping("/jpa/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username) {
-        return todoService.findAll();
+        return todoJpaRepository.findByUsername(username);
+        // return todoService.findAll();
     }
 
     // DELETE mapping
-    @DeleteMapping("/users/{username}/todos/{id}")
+    @DeleteMapping("/jpa/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
-        Todo todo = todoService.deleteById(id);
-
-        if (todo != null) {
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
+        // Todo todo = todoService.deleteById(id);
+        todoJpaRepository.deleteById(id);
+        // if (todo != null) {
+        //     return ResponseEntity.noContent().build();
+        // }
+        return ResponseEntity.noContent().build();
+        // return ResponseEntity.notFound().build();
 
     }
 
 
     // GET mapping for a particular id
-    @GetMapping("/users/{username}/todos/{id}")
+    @GetMapping("/jpa/users/{username}/todos/{id}")
     public Todo getTodo(@PathVariable String username, @PathVariable long id) {
-        return todoService.findById(id);
+        return todoJpaRepository.findById(id).get();
+        // return todoService.findById(id);
     }
 
 
     // PUT mapping
-    @PutMapping("/users/{username}/todos/{id}")
+    @PutMapping("/jpa/users/{username}/todos/{id}")
     public ResponseEntity<Todo> updateTodo(@PathVariable String username,
                                            @PathVariable long id, @RequestBody Todo todo) {
-        Todo updatedTodo = todoService.save(todo);
+        Todo updatedTodo = todoJpaRepository.save(todo);
 
         return new ResponseEntity<Todo>(todo, HttpStatus.OK);
     }
 
-    @PostMapping("/users/{username}/todos")
+    @PostMapping("/jpa/users/{username}/todos")
     public ResponseEntity<Void> createTodo(
             @PathVariable String username,
             @RequestBody Todo todo) {
 
-        Todo createdTodo = todoService.save(todo);
-
+        // Todo createdTodo = todoService.save(todo);
+        todo.setUsername(username);
+        Todo createdTodo = todoJpaRepository.save(todo);
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
@@ -66,7 +73,7 @@ public class TodoResource {
         return ResponseEntity.created(uri).build();
     }
 
-    @GetMapping("/hello-world/{name}")
+    @GetMapping("/jpa/hello-world/{name}")
     public String helloWorld(@PathVariable String name) {
         return "Hello World v1 from " + name + " from intellij";
     }
--- backend/demo/src/main/resources/data.sql
+++ backend/demo/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into todo(id, username, description, target_date, is_done)
 values
-(1001, 'suhail', 'learn to code', current_date(), false),
-(1002, 'akhtar', 'learn to be happy', current_date(), false),
-(1003, 'suhail akhtar', 'gotta run', current_date(), false);
+(1001, 'in28minutes', 'learn to code', current_date(), false),
+(1002, 'in28minutes', 'learn to be happy', current_date(), false),
+(1003, 'in28minutes', 'gotta run', current_date(), false);
