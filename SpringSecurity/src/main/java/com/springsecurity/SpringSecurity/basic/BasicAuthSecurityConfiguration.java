baa1e79035e440f8843535570302c82fb8804e09
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-05 23:24:55 +0530

    csrf token disabled

---
+++ SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/basic/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.springsecurity.SpringSecurity.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthSecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        // http.formLogin();
+        http.sessionManagement(
+                session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+        http.httpBasic();
+        http.csrf().disable();
+        return http.build();
+    }
+
+}
