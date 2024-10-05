47dac76bde414c9672e0bc5442fd76a80316e66d
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-05 19:51:38 +0530

    hello world resource moved to correct position

--- SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/resource/HelloWorldResource.java
+++ SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/resource/HelloWorldResource.java
@@ -1,4 +1,4 @@
-package com.springsecurity.SpringSecurity;
+package com.springsecurity.SpringSecurity.resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
