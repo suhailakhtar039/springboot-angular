c2d9f0f2ffdb57d721a9f79af39ee1b0b25058f9
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-05 23:34:40 +0530

    roles created

--- SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/basic/BasicAuthSecurityConfiguration.java
+++ SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/basic/BasicAuthSecurityConfiguration.java
@@ -4,22 +4,40 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().anyRequest().authenticated();
         // http.formLogin();
         http.sessionManagement(
-                session->
+                session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
         http.httpBasic();
         http.csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("suhail")
+                .password("{noop}abc")
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
