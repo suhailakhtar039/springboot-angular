9fc5e1096d209f7101f6e87c1b7bd14d5c821e55
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-06 15:27:38 +0530

    embedded database completed

--- SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/basic/BasicAuthSecurityConfiguration.java
+++ SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/basic/BasicAuthSecurityConfiguration.java
@@ -2,12 +2,18 @@ package com.springsecurity.SpringSecurity.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -24,6 +30,14 @@ public class BasicAuthSecurityConfiguration {
         http.csrf().disable();
         http.headers().frameOptions().sameOrigin();
         return http.build();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
     }
 
     @Bean
