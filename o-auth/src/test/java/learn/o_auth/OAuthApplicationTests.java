0bb4651dd2da12f2053932561e0bc4272295a6e4
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-07 15:56:09 +0530

    starting OAuth

---
+++ o-auth/src/test/java/learn/o_auth/OAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.o_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
