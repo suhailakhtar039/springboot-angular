315a46c0b32f82e1347b4867d2db0472d6d69b4d
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-07 15:50:20 +0530

    used secured annotation

--- SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/resource/TodoResource.java
+++ SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/resource/TodoResource.java
@@ -3,6 +3,7 @@ package com.springsecurity.SpringSecurity.resource;
 import jakarta.annotation.security.RolesAllowed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,7 @@ public class TodoResource {
     @PreAuthorize("hasRole('USER') and #username==authentication.name")
     @PostAuthorize("returnObject.username == 'suhail'")
     @RolesAllowed({"ADMIN","USER"})
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     public Todo retrieveTodoForUser(@PathVariable String username){
 
         return TODOS_LIST.get(0);
