05cd33ee949fa767b2a9c1089e014f7c26c0f514
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-07 11:25:48 +0530

    JWK source created

--- SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/jwt/JwtAuthSecurityConfiguration.java
+++ SpringSecurity/src/main/java/com/springsecurity/SpringSecurity/jwt/JwtAuthSecurityConfiguration.java
@@ -1,6 +1,12 @@
 package com.springsecurity.SpringSecurity.jwt;
 
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
@@ -22,6 +28,7 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPublicKey;
+import java.util.List;
 import java.util.UUID;
 
 @Configuration
@@ -90,7 +97,20 @@ public class JwtAuthSecurityConfiguration {
                 .keyID(UUID.randomUUID().toString())
                 .build();
     }
-    
+
+    @Bean
+    public JWKSource jwkSource(RSAKey rsaKey){
+        JWKSet jwkSet = new JWKSet(rsaKey);
+
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+
+        //         new JWKSource<>() {
+        //     @Override
+        //     public List<JWK> get(JWKSelector jwkSelector, SecurityContext securityContext) throws KeySourceException {
+        //         return jwkSelector.select(jwkSet);
+        //     }
+        // };
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
